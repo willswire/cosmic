d01a0893db66f454bbcb539024d04263ab83d420
William Walker <w_walker@icloud.com>
2024-09-21 15:25:44 -0500

    feat: setup command (#17)
    * setup command
    
    * add functional tests for setup
    
    * manual path update
    
    * prompt for successive setup

--- CosmicTests/CosmicTests.swift
+++ CosmicTests/CosmicTests.swift
@@ -5,12 +5,32 @@
 //  Created by Will Walker on 9/16/24.
 //
 
+import ArgumentParser
 import PklSwift
 import Testing
 
-@testable import Cosmic
+@testable import cosmic
 
-struct CosmicTests {
+struct CosmicSetupTests {
+    @Test("Set up Cosmic")
+    func setUpCosmic() async throws {
+        var setupCommand = Cosmic.Setup()
+        setupCommand.options = try .parse(["--verbose", "--force"])
+
+        try setupCommand.createPackagesDirectory()
+
+        let pklURL = try await setupCommand.downloadPkl()
+        #expect(pklURL.isFileURL)
+
+        try setupCommand.installPkl(from: pklURL)
+
+        let isProfileModified = try setupCommand.modifyShellProfiles()
+        // TODO: Test against output (which instructs users to self modify)
+        #expect(isProfileModified || true)
+    }
+}
+
+struct CosmicAddTests {
     @Test(
         "Install a package",
         arguments: [
@@ -26,30 +46,25 @@ struct CosmicTests {
             //"sops",
         ])
     func installPackage(packageName: String) async throws {
-        let addCommand = Cosmic.Add()
+        var addCommand = Cosmic.Add()
+        addCommand.options = try .parse(["--verbose"])
 
         let package = try await Package.loadFrom(
             source: .url(addCommand.packageManifestPath(for: packageName)))
-        print("parsed package: \(package.name)")
         #expect(package.name == packageName)
 
         let downloadLocation = try await addCommand.download(package: package)
-        print("package downloaded to: \(downloadLocation.path)")
         #expect(downloadLocation.isFileURL)
 
         try addCommand.validate(package: package, at: downloadLocation)
-        print("package is valid")
 
         let unpackedLocation = try addCommand.unpack(package: package, at: downloadLocation)
-        print("package unpacked to: \(unpackedLocation.path)")
         #expect(unpackedLocation.isFileURL)
 
         let exitCode = try addCommand.execute(package: package, at: unpackedLocation)
-        print("package exited with code: \(exitCode)")
         #expect(exitCode == 0)
 
         let isInstalled = try await addCommand.install(package: package, from: unpackedLocation)
-        print("package installed: \(isInstalled)")
         #expect(isInstalled)
     }
 }
