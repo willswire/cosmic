1c4a5f264952b540ad3f2d4a903216ffd3a781e7
William Walker <w_walker@icloud.com>
2024-10-01 14:12:28 -0500

    feat: improve CLI output (#26)
    * better logging
    
    * chore: improve code coverage for add subcommand
    
    * feat: add preinstall script for installer
    
    * chore: update functional tests to reflect setup changes

--- Cosmic/Cosmic.swift
+++ Cosmic/Cosmic.swift
@@ -14,10 +14,9 @@ import PklSwift
 @main struct Cosmic: AsyncParsableCommand {
     static var configuration = CommandConfiguration(
         abstract: "A package manager for macOS.",
-        subcommands: [Setup.self, Add.self])
+        subcommands: [Add.self])
 
     struct Options: ParsableArguments {
-        @Flag(name: [.long, .customShort("v")]) var verbose: Bool = false
-        @Flag(name: [.long, .customShort("f")]) var force: Bool = false
+        @Flag var verbose: Bool = false
     }
 }
